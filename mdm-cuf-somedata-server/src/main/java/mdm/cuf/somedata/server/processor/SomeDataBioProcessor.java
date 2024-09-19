ebb097095b2dd42392909db76ee98a5840be1b38
Oswald Plazola <plazolas@yahoo.com>
2024-09-19 14:04:44 -0400

    add schelon data change log handle

--- mdm-cuf-somedata-server/src/main/java/mdm/cuf/somedata/server/processor/SomeDataBioProcessor.java
+++ mdm-cuf-somedata-server/src/main/java/mdm/cuf/somedata/server/processor/SomeDataBioProcessor.java
@@ -2,17 +2,21 @@ package mdm.cuf.somedata.server.processor;
 
 import mdm.cuf.core.api.CufPushRequest;
 import mdm.cuf.core.messages.Message;
+import mdm.cuf.core.server.processor.AbstractBioProcessor;
 import mdm.cuf.somedata.server.dio.repository.SomeDataDioRepository;
 import mdm.cuf.somedata.server.processor.transformer.SomeDataBioDioTransformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import mdm.cuf.somedata.bio.SomeDataBio;
 import mdm.cuf.somedata.server.dio.SomeDataDio;
-import mdm.cuf.core.server.processor.AbstractBioProcessor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 /**
  * class to add extra business logic in case extra functionality is needed in top of core
@@ -28,29 +32,40 @@ public class SomeDataBioProcessor extends AbstractBioProcessor<SomeDataBio, Some
     @Autowired
     SomeDataBioDioTransformer someDataBioDioTransformer;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SomeDataBioProcessor.class);
+
     @Override
     public List<Message> pushExecutePreProcessing(CufPushRequest<SomeDataBio> request, Class<?>... validationHints) {
-
         final List<Message> errorList = new ArrayList<>();
 
-        SomeDataBio bio = request.getBio();
-        SomeDataDio dio = someDataBioDioTransformer.bioToDio(bio);
-        long id = dio.getSomeDataId();
-        SomeDataDio foundDio = someDataDioRepository.findById(id);
-        if(foundDio == null) {
-            setSomeDataDio(request.getBio());
-        }
+        mdm.cuf.core.bio.Bio coreBio = request.getBio();
+        SomeDataBio bio = new SomeDataBio();
+        bio.setSourceSystem(coreBio.getSourceSystem());
+        bio.setOriginatingSourceSystem(coreBio.getOriginatingSourceSystem());
+        bio.setSourceSystemUser(coreBio.getSourceSystemUser());
+        bio.setCreateDate(coreBio.getSourceDate());
+        bio.setSourceDate(coreBio.getSourceDate());
+        bio.setUpdateDate(coreBio.getSourceDate());
+
+        setSomeDataDio(bio);
+
         return errorList;
     }
 
     protected void setSomeDataDio(SomeDataBio bio){
         SomeDataDio dio = new SomeDataDio();
-        dio.setSomeDataId(bio.getSomeDataId());
+        long id = new Random().nextLong();
+        id = (id < 0) ? -id : id;
+        dio.setSomeDataId(id);
+        String desc = "demo" + dio.getSomeDataId().toString();
+        dio.setDemoField(desc);
         dio.setOriginatingSourceSystem(bio.getOriginatingSourceSystem());
         dio.setSourceDate(bio.getSourceDate());
         dio.setSourceSystem(bio.getSourceSystem());
         dio.setSourceSystemUser(bio.getSourceSystemUser());
-        dio.setTxAuditId(bio.getTxAuditId());
+        UUID uuid = UUID.randomUUID();
+        String uuidAsString = uuid.toString();
+        dio.setTxAuditId(uuidAsString);
         dio.setCreateDate(bio.getCreateDate());
         dio.setUpdateDate(bio.getUpdateDate());
         someDataDioRepository.save(dio);
---
+++ mdm-cuf-somedata-server/src/main/java/mdm/cuf/somedata/server/processor/async/SomeDataChangeLogHandler.java
@@ -0,0 +1,38 @@
+package mdm.cuf.somedata.server.processor.async;
+
+import mdm.cuf.core.api.async.AsyncStatus;
+import mdm.cuf.core.api.async.CufAsyncHandlerResponse;
+import mdm.cuf.core.api.async.CufChangeLogInstruction;
+import mdm.cuf.core.bio.Bio;
+import mdm.cuf.core.server.logging.LogUtil;
+import mdm.cuf.core.server.processor.async.changelog.ChangeLogHandler;
+import mdm.cuf.somedata.bio.SomeDataBio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+/**
+ * A PendingUpdateErrorHandler implementation that will receive CufPendingUpdateInstruction messages from the pending update error topic.
+ *
+ * @author jshrader
+ */
+@Component
+@Primary
+public class SomeDataChangeLogHandler implements ChangeLogHandler {
+
+    /** The Constant LOGGER. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(SomeDataChangeLogHandler.class);
+
+    @Override
+    public CufAsyncHandlerResponse handle(CufChangeLogInstruction cufChangeLogInstruction) {
+        Bio bio = cufChangeLogInstruction.getBio();
+        String bioStr = (null == bio) ? "no bio." : bio.toString();
+        LogUtil.logInfoWithBanner(LOGGER, "SomeData Change Log Handler",
+                "The following CufPendingUpdateInstruction was RECEIVED in this handler with bio: " + bioStr);
+        CufAsyncHandlerResponse response = new CufAsyncHandlerResponse();
+        response.setStatus(AsyncStatus.COMPLETED_SUCCESS);
+        return response;
+    }
+
+}
